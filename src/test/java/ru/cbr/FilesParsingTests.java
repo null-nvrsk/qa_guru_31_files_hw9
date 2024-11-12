96a12cf503481f3af376cc911636a9a3c9865207
Maksim Skomorokhov <nullnvrsk@gmail.com>
2024-11-12 21:51:02 +0300

    Added test xlsFileParsing

--- src/test/java/ru/cbr/FilesParsingTests.java
+++ src/test/java/ru/cbr/FilesParsingTests.java
@@ -1,10 +1,12 @@
 package ru.cbr;
 
 import com.codeborne.pdftest.PDF;
-import org.assertj.core.api.Assertions;
+import com.codeborne.xlstest.XLS;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-import java.io.*;
+import java.io.IOException;
+import java.io.InputStream;
 import java.util.zip.ZipEntry;
 import java.util.zip.ZipInputStream;
 
@@ -12,7 +14,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 public class FilesParsingTests {
 
-    private ClassLoader cl = FilesParsingTests.class.getClassLoader();
+    private final ClassLoader cl = FilesParsingTests.class.getClassLoader();
 
     private InputStream extractFileFromZip(String fileNameToExtract, String zipFilePath) throws IOException {
         ZipInputStream zis = new ZipInputStream(cl.getResourceAsStream(zipFilePath));
@@ -25,8 +27,8 @@ public class FilesParsingTests {
         return InputStream.nullInputStream();
     }
 
-
     @Test
+    @DisplayName("Проверка отчета службы по ЗПП на 09.2024")
     void pdfFileParsingTest() throws Exception {
         try (InputStream pdfInputStream = extractFileFromZip("report_2024_3.pdf", "report.zip")) {
             PDF pdf = new PDF(pdfInputStream);
@@ -38,5 +40,34 @@ public class FilesParsingTests {
         }
     }
 
+    @Test
+    @DisplayName("Проверка отчета по процентным ставка на 11.2024")
+    void xlsFileParsingTest() throws Exception {
+        try (InputStream xlsInputStream = extractFileFromZip("rates_table.xlsx", "report.zip")) {
+            XLS xls = new XLS(xlsInputStream);
 
+            assertThat(xls.excel.getSheet("2024").getRow(0).getCell(0).toString())
+                    .contains("Процентные ставки по операциям Банка России");
+
+            assertThat(xls.excel.getSheet("2024").getRow(2).getCell(5).toString())
+                    .contains("с 18.12.2023");
+            assertThat(xls.excel.getSheet("2024").getRow(3).getCell(5).toString())
+                    .isEqualTo("17.0");
+
+            assertThat(xls.excel.getSheet("2024").getRow(2).getCell(8).toString())
+                    .contains("с 28.10.2024");
+            assertThat(xls.excel.getSheet("2024").getRow(3).getCell(8).toString())
+                    .isEqualTo("22.0");
+            assertThat(xls.excel.getSheet("2024").getRow(4).getCell(8).toString())
+                    .isEqualTo("22.75");
+            assertThat(xls.excel.getSheet("2024").getRow(5).getCell(8).toString())
+                    .isEqualTo("21.25");
+            assertThat(xls.excel.getSheet("2024").getRow(8).getCell(8).toString())
+                    .isEqualTo("21.0");
+            assertThat(xls.excel.getSheet("2024").getRow(9).getCell(8).toString())
+                    .contains("ключевая ставка");
+            assertThat(xls.excel.getSheet("2024").getRow(10).getCell(8).toString())
+                    .isEqualTo("20.0");
+        }
+    }
 }
